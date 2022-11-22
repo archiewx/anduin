b348520136ca1755fd0a092fba7f6bf8e3d00ca8
archie.zheng <archie.zheng@nio.com>
2022-11-22 16:02:24 +0800

    feat: Installation dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { Button } from 'antd';
+import * as styled from './styled';
 import './App.css';
 
 function App() {
-  return <Button>Hello World</Button>;
+  return (
+    <>
+      <Button>Hello World</Button>
+      <styled.Box>Hello World</styled.Box>
+    </>
+  );
 }
 
 export default App;
